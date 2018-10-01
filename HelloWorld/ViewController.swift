d28655605b5e3cff83f407245c5c41608d30c1d2
Sagar Surana <Sagar@D-10-19-197-123.dhcp4.washington.edu>
2018-10-01 13:46:39 -0700

    all files

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sagar Surana on 9/26/18.
+//  Copyright © 2018 ischoolsagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSLog("ViewController loaded!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
